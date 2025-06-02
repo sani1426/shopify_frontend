f2f49cde12d0aeb6670e8bc60fe6e28561b8c400
saman <sam.karimi727@gmail.com>
2025-06-03 01:32:16 +0330

    update page.tsx

--- app/(Root)/page.tsx
+++ app/(Root)/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-<div className="bg_soft text-5xl text-rose-700">HOME PAGE
+<div className="bg_soft text-5xl text-rose-700 pt-24">HOME PAGE
     <button className="gradient_bg text-white px-4 py-3">click</button>
 </div>
   );
