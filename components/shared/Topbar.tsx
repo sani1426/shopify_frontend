b4bc4f0216b813643c65a56096d09cefb2fcbfd3
saman <sam.karimi727@gmail.com>
2025-06-04 13:30:13 +0330

    update Topbar.tsx

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -4,6 +4,7 @@ import { FaUserCircle } from 'react-icons/fa'
 import Link from 'next/link'
 import Logo from './logo'
 import ThemeToggle from '../UI/ThemeToggle'
+import { IoSearchCircleOutline } from 'react-icons/io5'
 
 
 
@@ -33,7 +34,7 @@ const Topbar = () => {
         type='submit'
         className='bg-yellow-500 text-black rounded-s-none rounded-e-md h-full px-3 py-2  hover:bg-yellow-300 duration-500 transition-all'
       >
-        <SearchIcon className='w-6 h-6' />
+        <IoSearchCircleOutline className='w-6 h-6' />
       </button>
         </div>
  
