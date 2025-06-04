09082c85643828f534439051cce2c211649b45a6
saman <sam.karimi727@gmail.com>
2025-06-04 13:29:41 +0330

    update Topbar.tsx

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -17,21 +17,27 @@ const Topbar = () => {
 
       className='h-20 bg_soft  shadow-md fixed w-full z-[1000]'
     >
-      <div className='container my-auto mx-auto  flex-center h-full px-4'>
-        <Link className='flex items-center justify-start' href='/'>
+      <div className='container my-auto mx-auto flex-between h-full gap-8 lg:gap-12   px-4'>
+        <Link className='flex-center ' href='/'>
           <Logo />
           <span className='text-3xl  text-nowrap'>Shopify</span>
         </Link>
 
         <div className={`w-full   bg_soft rounded-md hidden lg:flex items-center justify-center flex-grow `}>
           <input
-            className='w-full rounded-md px-4 py-2'
+            className='w-full rounded-e-none rounded-s-md  px-4 py-2'
             type='text'
             placeholder='search ....'
           />
+            <button
+        type='submit'
+        className='bg-yellow-500 text-black rounded-s-none rounded-e-md h-full px-3 py-2  hover:bg-yellow-300 duration-500 transition-all'
+      >
+        <SearchIcon className='w-6 h-6' />
+      </button>
         </div>
  
-        <div className='w-full flex items-center  gap-4 justify-end'>
+        <div className='w-full flex items-center justify-end  gap-4'>
      
 
               <Link
