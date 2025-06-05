e5f1d30079dee96d5346db0f127e2fc7ab1aad41
saman <sam.karimi727@gmail.com>
2025-06-06 03:19:34 +0330

    update HeroCarousel.tsx and component.css

--- components/UI/HeroCarousel.tsx
+++ components/UI/HeroCarousel.tsx
@@ -5,18 +5,18 @@ import './component.css'
 
 const HeroCarousel = () => {
   return (
-    <div className='carousel w-[100vw] h-[100vh] overflow-hidden relative'>
+    <div className='carousel '>
       <div className='list'>
         {sliderData.map((item) => {
           return (
-            <div key={item?.id} className='item absolute inset-0'>
-              <img src={item?.imgSrc} alt='' className='w-full h-full object-cover' />
-              <div className='content absolute top-[20%] w-[1140px] max-w-[80%] left-[50%] transform -translate-x-[50%] pr-[30%] box-border text-[#fff] '>
-                <div className='author font-bold tracking-[10px] '>{item?.title}</div>
-                <div className='title font-bold text-[5em] '>{item?.offer}</div>
-                <div className='topic font-bold text-[5em] text-[#f1683a]'>saman</div>
+            <div key={item?.id} className='item '>
+              <img src={item?.imgSrc} alt='' className='' />
+              <div className='content  '>
+                <div className='author '>{item?.title}</div>
+                <div className='title  '>{item?.offer}</div>
+                <div className='topic'>saman</div>
                 <div className='des'>{item?.description}</div>
-                <div className='buttons grid grid-cols-[repeat(2,130px)] grid-rows-[40px] gap-1 mt-5'>
+                <div className='buttons'>
                   <button className='border-none bg-[#eee] tracking-[3px] font-semibold '>{item?.buttonText1}</button>
                   <button className='tracking-[3px] font-semibold border-[1px] border-white bg-transparent text-white'>{item?.buttonText2}</button>
                 </div>
@@ -26,14 +26,14 @@ const HeroCarousel = () => {
         })}
       </div>
 
-        <div className="thumbnail absolute top-[50%] left-[50%] w-[max-content] z-[100] flex gap-5">
+        <div className="thumbnail ">
             {
                 sliderData.map((_) => {
                     return(
-                        <div className="item w-[150px] h-[220px] flex-shrink-0 relative">
-                        <img src={_?.imgSrc} alt="" className='w-full h-full object-cover rounded-[20px]' />
-                        <div className="content absolute bottom-[10px] left-[10px] right-[10px]">
-                            <div className=" author font-bold">{_?.title}</div>
+                        <div className="item ">
+                        <img src={_?.imgSrc} alt="" className='' />
+                        <div className="content ">
+                            <div className=" author ">{_?.title}</div>
                             <div className="des">{_?.description}</div>
                         </div>
                     </div>
