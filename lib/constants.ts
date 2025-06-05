7111eebfd82efaca2b4ce0b4072f96cde937b687
saman <sam.karimi727@gmail.com>
2025-06-05 13:18:06 +0330

    update 1 file, create 3 files and move 3 files

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,30 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shopify'
      process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
      'An Online Shop built with Next.js and MongoDB'
 
+
+     const sliderData = [
+      {
+        id: 1,
+        title: "Experience Pure Sound - Your Perfect Headphones Awaits!",
+        offer: "Limited Time Offer 30% Off",
+        buttonText1: "Buy now",
+        buttonText2: "Find more",
+        imgSrc: '/carousel/bose_headphone_image.png'
+      },
+      {
+        id: 2,
+        title: "Next-Level Gaming Starts Here - Discover PlayStation 5 Today!",
+        offer: "Hurry up only few lefts!",
+        buttonText1: "Shop Now",
+        buttonText2: "Explore Deals",
+        imgSrc: '/carousel/banner2.jpg'
+      },
+      {
+        id: 3,
+        title: "Power Meets Elegance - Apple MacBook Pro is Here for you!",
+        offer: "Exclusive Deal 40% Off",
+        buttonText1: "Order Now",
+        buttonText2: "Learn More",
+        imgSrc: '/carousel/banner1.jpg'
+      },
+    ];
